3f802a2313c9863307dd6fe53a66df22f8899b76
endsoul <kkw855@gmail.com>
2020-08-04 14:39:59 +0900

    lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import ReactDOM from "react-dom"
-import React from "react";
+import ReactDOM from 'react-dom';
+import React from 'react';
 
 export type Props = { text: string };
 
-const App = (
-    <div>Hello World</div>
-);
+const App = <div>Hello World</div>;
 
 ReactDOM.render(App, document.getElementById('root'));
