af8758ea20a614dd1b6ecbc5cad4eab2a3eed22e
endsoul <kkw855@gmail.com>
2020-09-18 17:44:10 +0900

    react-icons 패키지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { hot } from 'react-hot-loader/root';
 import React, { ReactElement } from 'react';
 import Counter from './Counter';
 import './style.scss';
+import MyComponent from './MyComponent';
 
 function App(): ReactElement {
   return (
     <div>
+      <MyComponent />
       <h1 style={{ fontFamily: 'Nanum Myeongjo' }}>고장난 시계</h1>
       <Counter />
     </div>
